86d0061bf0cd147a153b9747bf82b433f86fe174
lucasdicesaro <lucasdicesaro@529a5822-bcd2-1923-d83c-3349785f8a36>
2010-06-11 04:14:48 +0000

    restructuracion ubicacion de diag secuencia

--- trunk/documentacion/Diseño/html/tocTab.js
+++
@@ -1,5 +0,0 @@
-var tocTab = new Array();
-tocTab[0] = new Array ("0", "Class Model", "EARoot/contents.html", "");
-tocTab[1] = new Array ("1", "System", "EARoot/EA1/contents.html", "");
-tocTab[2] = new Array ("2", "Frameworks", "EARoot/EA2/contents.html", "");
-var nCols = 20;
